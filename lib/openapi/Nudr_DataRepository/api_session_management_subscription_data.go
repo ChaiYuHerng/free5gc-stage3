025a6ba197e08bd9adcf51fc556114f2246b28ef
chris60303 <chris60303@gmail.com>
2020-10-22 12:31:56 +0800

    several dnn

--- lib/openapi/Nudr_DataRepository/api_session_management_subscription_data.go
+++ lib/openapi/Nudr_DataRepository/api_session_management_subscription_data.go
@@ -64,7 +64,7 @@ func (a *SessionManagementSubscriptionDataApiService) QuerySmData(ctx context.Co
 		localVarReturnValue  []models.SessionManagementSubscriptionData
 	)
 
-	fmt.Printf("now in the QuerySmData,localVarReturnValue is %s\n",localVarReturnValue)
+	fmt.Printf("now in the QuerySmData,localVarReturnValue is %s\n",&localVarReturnValue)
 
 	// create path and map variables
 	localVarPath := a.client.cfg.BasePath() + "/subscription-data/{ueId}/{servingPlmnId}/provisioned-data/sm-data"
