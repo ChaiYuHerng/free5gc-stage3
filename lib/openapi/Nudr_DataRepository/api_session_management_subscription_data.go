660ee565ad2fb0a40cd2a2f939ba0fb8d42f0286
chris60303 <chris60303@gmail.com>
2020-10-27 17:59:55 +0800

    several dnn

--- lib/openapi/Nudr_DataRepository/api_session_management_subscription_data.go
+++ lib/openapi/Nudr_DataRepository/api_session_management_subscription_data.go
@@ -76,12 +76,13 @@ func (a *SessionManagementSubscriptionDataApiService) QuerySmData(ctx context.Co
 	localVarQueryParams := url.Values{}
 	localVarFormParams := url.Values{}
 
-	fmt.Printf("localVarQueryParams is %s\n\n",localVarQueryParams)
+	fmt.Printf("1st localVarQueryParams is %s\n\n",localVarQueryParams)
 	fmt.Printf("1st len(localVarQueryParams) is %d\n\n",len(localVarQueryParams))
 
 	if localVarOptionals != nil && localVarOptionals.SingleNssai.IsSet() {
 		fmt.Printf("test1\n")
 		localVarQueryParams.Add("single-nssai", openapi.ParameterToString(localVarOptionals.SingleNssai.Value(), ""))
+		fmt.Printf("2nd localVarQueryParams is %s\n\n",localVarQueryParams)
 		fmt.Printf("2nd len(localVarQueryParams) is %d\n\n",len(localVarQueryParams))
 	}
 	if localVarOptionals != nil && localVarOptionals.Dnn.IsSet() {
